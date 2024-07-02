4d7a0513a816316260615be47e7814df782f2d21
abuelhijarwad <abuelhijarwad@gmail.com>
2024-07-03 00:52:25 +0300

    sdsad

--- server.js
+++ server.js
@@ -10,19 +10,27 @@ const io = socketIo(server);
 
 const PORT = 3000;
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', socket => {
   console.log('New user connected');
 
   socket.on('requestSound', () => {
     const filePath = path.join(__dirname, 'sound.mp3');
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        console.error('Error reading sound file:', err);
-        socket.emit('error', 'Error reading sound file');
-        return;
-      }
-      socket.emit('soundFile', data);
+    const readStream = fs.createReadStream(filePath);
+
+    readStream.on('data', chunk => {
+      socket.emit('soundChunk', chunk);
+    });
+
+    readStream.on('end', () => {
+      socket.emit('soundEnd');
+    });
+
+    readStream.on('error', err => {
+      console.error('Error reading sound file:', err);
+      socket.emit('error', 'Error reading sound file');
     });
   });
 
